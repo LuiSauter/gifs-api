86c52010d5f515964737c31e2735795d1b30ade9
Luis Gabriel <janco7249@gmail.com>
2021-10-28 15:29:06 -0400

    refactor: changes routes

--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,19 @@
 const { Router } = require('express')
 const router = Router()
 
-const { createUser } = require('../controllers/user.controller')
+const { createUser, getUser } = require('../controllers/register.controller')
 
 router.route('/')
   .post(createUser)
+
+router.route('/users')
+  .get(getUser)
+
 /**
  * trabajando en ello
  */
 // router.route('/:id')
 //   .put(updateUser)
 //   .delete(deleteUser)
+
+module.exports = router
