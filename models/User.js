de4735aa9a6b4b97e4530a585098a62921f1084a
Luis Gabriel <janco7249@gmail.com>
2021-10-27 23:36:53 -0400

    create models schema for users

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const uniqueValidator = require('mongoose-unique-validator')
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  favs: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Favs'
+  }]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+const User = model('User', userSchema)
+
+module.exports = User
