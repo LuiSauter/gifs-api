4507ec3cf19e6eb4864bc0ae76da45a871bca368
Luis Gabriel <janco7249@gmail.com>
2021-11-01 20:52:00 -0400

    add express session and passport initializations

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('./loggerMiddleware')
-
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+// Initializations
 const app = express()
+require('./config/passport')
 
 const notFound = require('./middleware/notFound')
 const handleErrors = require('./middleware/handleErrors')
-
 /**
- * middleware
+ * Middleware
  */
 app.use(cors())
 app.use(express.json())
 app.use(logger)
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 /**
- * for template
+ * Global Variables
  */
-// app.use(express.static('./template/example'))
-
+app.use((req, res, next) => {
+  res.locals.error = req.flash('error')
+  res.locals.user = req.user || null
+  next()
+})
 /**
- * routes
+ * Routes
  */
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {// no template
   res.send('<div><h1 style="color: #123; font-family: sans-serif">GIF API</h1></div>')
 })
-app.use('/api/register', require('./routes/register'))
-app.use('/api/login', require('./routes/login'))
-app.use('/api/favs', require('./routes/favs'))
-
+app.use('/api/users', require('./routes/user.routes'))
+app.use('/api/favs', require('./routes/favs.routes'))
 /**
  * routes test
  */
 if (process.env.NODE_ENV === 'test') {
   app.use('api/testing', require(''))
 }
-
 /**
  * error handling
  */
