a7a695f9ca8f2e38a042cda767a02d8a82940f92
Luis Gabriel <janco7249@gmail.com>
2021-11-01 20:52:53 -0400

    add passport math email and password

---
+++ config/passport.js
@@ -0,0 +1,32 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/User')
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, async (email, password, done) => {
+  // Math Email's user
+  const user = await User.findOne({ email })
+  if (!user) {
+    return done(null, false, { message: 'Not User found' })
+  } else {
+    // Math Password's User
+    const match = await user.matchPassword(password)
+    if (match) {
+      return done(null, user)
+    } else {
+      return done(null, false, { message: 'Incorrect Password'})
+    }
+  }
+}))
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user)
+  })
+})
