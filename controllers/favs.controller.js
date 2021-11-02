8e995254443bf7fd37690c9f2cda50e669ea7da8
Luis Gabriel <janco7249@gmail.com>
2021-11-01 20:54:35 -0400

    refactor: changes authentication all request

--- controllers/favs.controller.js
+++ controllers/favs.controller.js
@@ -1,48 +1,42 @@
-const UserModel = require('../models/User')
-const favsCtrl = {}
+// const UserModel = require('../models/User')
 const FavModel = require('../models/Fav')
+const favsCtrl = {}
 
-favsCtrl.getFavs = async (req, res) => {
-  const favs = await FavModel.find({}).populate('user', {
-    username: 1,
-    _id: 0
-  })
+favsCtrl.getAllFavs = async (req, res) => {
+  const favs = await FavModel.find({ user: req.user.id }).sort({ createdAt: 'desc' })
+  console.log('************DEBUG-GETALLFAVS************')
+  console.log(req.user)
+  console.log('************DEBUG-GETALLFAVS************')
   res.json(favs)
 }
 
-favsCtrl.postFavs = async (req, res, next) => {
+favsCtrl.createFav = async (req, res) => {
   const { fav } = req.body
-  const { userId } = req
-  const user = await UserModel.findById(userId)
-  // const favorites = await FavModel.find({})
-  // const alreadyExist = favorites.some(favM => favM === fav)
+  console.log(fav)
   if (!fav) {
-    return res.status(404).json({ error: 'required "fav" field is missing' })
+    return res.status(404).json({ message: false })
   }
-  const newFav =new FavModel({
-    fav,
-    user: user._id
-  })
-  try {
-    const savedFav = await newFav.save()
-    console.log(savedFav._id, 'SAVEDFAV ID')
-    user.favs = user.favs.concat(savedFav._id)
-    await user.save()
-    res.json(savedFav)
-  } catch (error) {
-    next(error)
-  }
-}
 
-favsCtrl.deleteFavs = async (req, res, next) => {
-  try {
-    const { id } = req.params
-    console.log(req.params, 'help Debug')
-    await FavModel.findByIdAndDelete(id)
-    res.status(204).end()
-  } catch (error) {
-    next(error)
+  const favs = await FavModel.findOne({ user: req.user.id, fav: fav })
+
+  if (favs !== null) {
+    await FavModel.findByIdAndDelete(favs._id)
+    return res.json({ message: true })
   }
+
+  const newFav = new FavModel({ fav })
+  newFav.user = req.user.id
+  const favSaved = await newFav.save()
+  console.log('************USER************')
+  console.log(favSaved)
+  console.log('************USER************')
+
+  const resFav = {
+    fav: favSaved.fav,
+    id: favSaved._id,
+    message: true
+  }
+  res.status(201).json(resFav)
 }
 
 module.exports = favsCtrl
--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,41 +1,65 @@
-const UserModel = require('../models/User')
-const bcrypt = require('bcrypt')
+const User = require('../models/User')
+const FavModel = require('../models/Fav')
 const usersCtrl = {}
 
-usersCtrl.getUser = async (req, res) => {
-  const users = await UserModel.find({}).populate('favs', {
-    fav: 1
-  })
-  res.json(users)
-}
-
-usersCtrl.createUser = async (req, res) => {
-  try {
-    const { body } = req
-    const { username, password } = body
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-
-    if (!username) {
-      return res.status(400).json({ error: 'required "username" field is missing' })
-    }
-    // register user
-    const newUser = new UserModel({
-      username,
-      passwordHash
-    })
-    const saveUser = await newUser.save()
-    res.status(201).json(saveUser)
-  } catch (error) {
-    res.status(400).json(error)
+usersCtrl.signup = async (req, res) => {
+  const { name, email, password, confirm_password } = req.body
+  const newUser = {
+    email,
   }
+  console.log('************bodyRegister************')
+  console.log(req.body)
+  console.log('************bodyRegister************')
+  if (password !== confirm_password) {
+    newUser.password = password
+    newUser.confirm_password = confirm_password
+    newUser.name = name
+    newUser.message = 'Passwords do not match'
+    return res.status(400).json(newUser)
+  }
+  if (password.length < 4) {
+    newUser.password = password
+    newUser.confirm_password = confirm_password
+    newUser.name = name
+    newUser.message = 'Passwords must be at least 4 characters'
+    return res.status(400).json(newUser)
+  }
+
+  const emailUser = await User.findOne({ email: email })
+
+  if (emailUser) {
+    newUser.password = password
+    newUser.confirm_password = confirm_password
+    newUser.name = name
+    newUser.message = 'The email is already in use.'
+    return res.status(400).json(newUser)
+  }
+  const newObjectUser = new User({ name, email, password })
+  newObjectUser.password = await newObjectUser.encryptPassword(password)
+  await newObjectUser.save()
+
+  console.log('**********SIGNUP**********')
+  console.log(newObjectUser)
+  console.log('**********SIGNUP**********')
+  newUser.message = 'Signup Successfuly!'
+  res.send(newUser)
 }
 
-/**
- * trabajando en ello
- */
-// usersCtrl.updateUser = async (req, res) => {}
-// usersCtrl.deleteUser = async (req, res) => {}
+usersCtrl.signin = async (req, res) => {
+  console.log('************DEBUG-SIGNIN************')
+  console.log({ message: true })
+  console.log('************DEBUG-SIGNIN************')
+  res.json({ message: true})
+}
+
+usersCtrl.logout = async (req, res) => {
+  req.logout()
+  res.json({ message: 'You are logged out now.' })
+}
+
+usersCtrl.deleteUser = async (req, res) => {
+  await FavModel.deleteMany({ user: req.user.id })
+  res.status(204).end()
+}
 
 module.exports = usersCtrl
