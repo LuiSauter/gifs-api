16a1223e95e7e93abde0dd5f76e10d2bd8a80151
Luis Gabriel <janco7249@gmail.com>
2021-11-03 11:23:06 -0400

    changes routes user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 
-const { signup, signin, logout, deleteUser } = require('../controllers/register.controller')
+const { signup, signin, deleteUser } = require('../controllers/register.controller')
 // const { isAuthenticated } = require('../helpers/auth')
 const useExtractor = require('../middleware/userExtractor')
 
@@ -9,7 +9,7 @@ router.route('/signup').post(signup)
 
 router.route('/signin').post(signin)
 
-router.route('/logout').get(logout)
+// router.route('/logout').get(logout)
 
 router.route('/delete').delete(useExtractor ,deleteUser)
 
