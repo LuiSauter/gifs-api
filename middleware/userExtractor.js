7e002242220be64fd3d5bb5d0f01c7b5a8fddddc
Luis Gabriel <janco7249@gmail.com>
2021-11-02 23:39:19 -0400

    changes req userId

--- middleware/userExtractor.js
+++ middleware/userExtractor.js
@@ -1,21 +1,23 @@
 const jwt = require('jsonwebtoken')
 // middleware que hace la extraccion del usuario y el token
 module.exports = (request, response, next) => {
-  const authorization = request.get('authorization')// header
-  let token = ''
-  console.log(authorization, 'AUTORIZACION')
+  // const authorization = request.get('authorization')// header
+  // let token = ''
+  const authorization = request.headers.authorization
+  if (!authorization) return response.status(401).send('Unthorized Request')
 
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    token = authorization.substring(7)
-  }
+  const token = authorization.split(' ')[1]
 
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  console.log(decodedToken, 'DECODEDTOKEN')
+  console.log('**********TOKEN**********')
+  console.log(token)
+  console.log('**********TOKEN**********')
 
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const { id: userId } = decodedToken
-  request.userId = userId
+  if (token === 'null') return response.status(401).send('Unthorized Request')
+
+  const payload = jwt.verify(token, process.env.SECRET)
+  console.log('**********TOKEN**********')
+  console.log(payload)
+  console.log('**********TOKEN**********')
+  request.user = payload._id
   next()
 }
